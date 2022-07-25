0ed32772123a1e7cd8ab624b263a3111558d47a6
iamemmax <68588402+iamemmax@users.noreply.github.com>
2022-07-25 21:39:27 +0100

    fix netlify erroror

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react"
+import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import { Container, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 
 import { theme } from "./component/config/theme";
 
-import LoginPage from './pages/LoginPage';
-import { Homepages } from './pages/Homepages';
+import LoginPage from "./pages/LoginPage";
+import { Homepages } from "./pages/Homepages";
 
 function App() {
-
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
         <Router>
-
           <div className="App">
             <main>
               {/* <Container> */}
